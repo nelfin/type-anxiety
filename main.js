4670b7846447e1c4a9a100073d23778551b61843
Andrew Haigh <nelfin@gmail.com>
2016-08-13 22:12:57 +1000

    Refactor bar methods into object

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
-function countdown(bar, duration) {
-  var updateInterval = 200;
-  var updates = duration * (1000 / updateInterval);
-  var timeRemaining = updates;
+var Bar = function(max, progressElem) {
+  this.value = max;
+  this.max = max;
+  this.elem = progressElem;
+};
+
+Bar.prototype.decrement = function(v) {
+  this.value = Math.max(this.value-v, 0);
+  this.elem.value = this.value/this.max;
+};
+
+function countdown(bar, updateIntervalMillis) {
+  var update = updateIntervalMillis / 1000;
   setInterval(function() {
-    bar.value = timeRemaining / updates;
-    if (--timeRemaining < 0) {
-      timeRemaining = 0;
-    }
-  }, updateInterval);
+    bar.decrement(update);
+  }, updateIntervalMillis);
 }
 
 window.onload = function() {
-  var bar = document.getElementById('anxiety-bar');
-  countdown(bar, 30);
+  var bar = new Bar(30, document.getElementById('anxiety-bar'));
+  countdown(bar, 200);
 };
